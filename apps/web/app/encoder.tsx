2164120140e84db93a539ca5645679e352bae638
promet99 <54811538+promet99@users.noreply.github.com>
2023-07-09 01:48:16 +0900

    feat: add encoder for qrVersion0Text

---
+++ apps/web/app/encoder.tsx
@@ -0,0 +1,108 @@
+/*
+Version 40M: 2334Byte (~2kb)
+A "Set" is a group of indexed QR codes that together holds a binary data.
+
+===Header===
+    ==Header For First Qr ==
+        [All] Protocol Name 3byte: QRP
+        Version 1byte: start from 0x00
+        Type 1byte (differ by Version): 
+            digit[1,2]: 0:text, 1:file
+            digit[3,4]: compress. (0: no compress, 1: lz-compress)
+            digit[5,6]: encrypt. (0: no encrypt, 1: encrypt)
+            digit[7,8]: ???
+        
+        [All] Hash: 2byte (make sure qr codes are of same set)
+        [All] QR Index: 1byte (0~255)
+    
+===Content===
+    Max 2320~2330 (=2334-??)Byte
+        If File: 
+            - File name length 1Byte
+            - File name ??Byte (utf16?)
+            - File extension 4Byte char
+            - File content ??Byte
+
+- Hash: last N bytes of Sha3-256 
+
+
+*/
+import { createHash } from "sha256-uint8array";
+
+const QR_40M_SIZE_BYTE = 2234;
+
+export const encodeToDataArrForQr = (
+  data:
+    | {
+        type: "text";
+        content: string;
+        //   encrypt?: boolean;
+        //   compress?: boolean;
+      }
+    | {
+        type: "file";
+        content: Uint8Array;
+        filename: string;
+        extension: string;
+      }
+): Uint8Array[] => {
+  const protocolName = new TextEncoder().encode("QRP");
+  const version = new Uint8Array([0]);
+  const type = new Uint8Array([0]);
+
+  //   FirstHeader.
+  if (data.type === "text") {
+    const encodedText = new TextEncoder().encode(data.content);
+    const hash: Uint8Array = createHash()
+      .update(encodedText)
+      .digest()
+      .slice(-2);
+
+    const FIRST_HEADER_SIZE = 8;
+    const FIRST_QR_MAX_SIZE = QR_40M_SIZE_BYTE - FIRST_HEADER_SIZE;
+    const firstHeader = new Uint8Array([
+      ...protocolName,
+      ...version,
+      ...type,
+      ...hash,
+      ...new Uint8Array([0]),
+    ]);
+
+    // length: 6
+    const REST_HEADER_SIZE = 6;
+    const REST_QR_MAX_SIZE = QR_40M_SIZE_BYTE - REST_HEADER_SIZE;
+    const makeHeaderForRestQr = (index: number) =>
+      new Uint8Array([...protocolName, ...hash, ...new Uint8Array([index])]);
+    const firstQrData = new Uint8Array([
+      ...firstHeader,
+      ...encodedText.slice(0, FIRST_QR_MAX_SIZE),
+    ]);
+
+    const qrCount =
+      Math.ceil((encodedText.length - FIRST_QR_MAX_SIZE) / REST_QR_MAX_SIZE) +
+      1;
+    if (qrCount > 1) {
+      const restData = encodedText.slice(QR_40M_SIZE_BYTE - FIRST_HEADER_SIZE);
+      const restQrArr = new Array(qrCount - 1)
+        .fill(null)
+        .map(
+          (_, i) =>
+            new Uint8Array([
+              ...makeHeaderForRestQr(i + 1),
+              ...restData.slice(
+                i * REST_QR_MAX_SIZE,
+                (i + 1) * REST_QR_MAX_SIZE
+              ),
+            ])
+        );
+
+      return [firstQrData, ...restQrArr];
+    } else {
+      //   One QR is enough.
+      return [firstQrData];
+    }
+  } else if (data.type === "file") {
+    type[0] = type[0] & 0b01000000;
+    return [new Uint8Array([])];
+  }
+};
--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,10 +1,107 @@
-import { Button, Header } from "ui";
+"use client";
+import QRCode from "qrcode";
+import { useEffect, useState } from "react";
+import {
+  Page,
+  Text,
+  View,
+  Document,
+  StyleSheet,
+  PDFViewer,
+  Image,
+} from "@react-pdf/renderer";
+import { encodeToDataArrForQr } from "./encoder";
+import { testQrData } from "./testData";
 
-export default function Page() {
+const styles = StyleSheet.create({
+  page: { backgroundColor: "white", marginTop: 10, marginBottom: 10 },
+  section: {
+    display: "flex",
+    flexWrap: "wrap",
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+  },
+  title: {
+    textAlign: "center",
+    width: "100%",
+  },
+  qrImgStyle: {
+    width: 270,
+    height: 270,
+    padding: 0,
+    margin: 0,
+    display: "flex",
+  },
+  block: { width: 250, height: 250, margin: 0, padding: 0 },
+});
+
+export default function MainPage() {
+  const [src, setSrc] = useState("");
+  const [src2, setSrc2] = useState("");
+
+  const doc = (
+    <Document>
+      <Page size="A4" style={styles.page}>
+        <View style={styles.section}>
+          <View style={styles.title}>
+            <Text>Section #1</Text>
+          </View>
+
+          <Image src={src} style={styles.qrImgStyle} />
+
+          <Image src={src} style={styles.qrImgStyle} />
+          <Image src={src} style={styles.qrImgStyle} />
+          <Image src={src} style={styles.qrImgStyle} />
+          <Image src={src} style={styles.qrImgStyle} />
+          <Image src={src} style={styles.qrImgStyle} />
+          <Image src={src} style={styles.qrImgStyle} />
+          <Image src={src} style={styles.qrImgStyle} />
+        </View>
+      </Page>
+    </Document>
+  );
+
+  useEffect(() => {
+    console.log(testQrData);
+    QRCode.toDataURL([{ data: testQrData[0], mode: "byte" }], {
+      version: 40,
+      scale: 2,
+      type: "image/png",
+    }).then((url) => {
+      setSrc(url);
+    });
+    QRCode.toDataURL([{ data: testQrData[1], mode: "byte" }], {
+      version: 40,
+      scale: 2,
+      type: "image/png",
+    }).then((url) => {
+      setSrc2(url);
+    });
+  }, []);
+
   return (
     <>
-      <Header text="Web" />
-      <Button />
+      <PDFViewer width={600} height={900}>
+        {doc}
+      </PDFViewer>
+
+      <img
+        src={src}
+        alt=""
+        style={{
+          width: 600,
+          height: 600,
+        }}
+      />
+      <img
+        src={src2}
+        alt=""
+        style={{
+          width: 600,
+          height: 600,
+        }}
+      />
     </>
   );
 }
